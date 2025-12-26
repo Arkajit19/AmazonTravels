8b46c0b9f4f0e35af5604a4bf88950df802da039
Arkajit <59366904+Arkajit19@users.noreply.github.com>
2025-12-27 03:29:04 +0530

    intial add

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Home from './components/Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from './components/Navbar';
+function App() {
+  
+  return (
+    <>
+      <NavbarComponent />
+     <Home />
+    </>
+  )
+}
+
+export default App

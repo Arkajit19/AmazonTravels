959e123bd3f5912209348b9a2874ef5818839974
Arkajit <59366904+Arkajit19@users.noreply.github.com>
2025-12-27 03:51:13 +0530

    today's last change

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,9 @@ import "./Home.css"
 const Home = () => {
   return (
     <div>
-        <h1>Welcome to the Home Page</h1>
+      
+      <h1>Welcome to the Home Page</h1>
+      
       </div>
   )
 }
